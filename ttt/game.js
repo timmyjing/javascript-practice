d22ad250602727140787e64196e88d52cdee0f8c
Timmy Jing <timmyjing@gmail.com>
2018-08-24 11:22:44 -0700

    Refactor for promise and async/await

--- ttt/game.js
+++ ttt/game.js
@@ -19,25 +19,67 @@ Game.prototype.switchTurn = function() {
   this.playerOneTurn = !this.playerOneTurn;
 }
 
-Game.prototype.run = function() {
-  let pos = rl.question('Enter a pos: ', answer => {
-    let pos = answer.split(",");
-    pos = [parseInt(pos[0]), parseInt(pos[1])];
-    let currPlayer = this.playerOneTurn ? "X" : "O";
-    try {
-      this.board.handleInput(currPlayer, pos);
-    } catch(err) {
-      console.log(err);
-      this.board.handleInput(currPlayer, pos);
-    }
-    this.switchTurn();
-    if (this.board.over) {
-      console.log('gg');
-    } else {
-      this.board.render();
-      this.run();
-    }
+// Game.prototype.run = function() {
+//   let pos = rl.question('Enter a pos: ', answer => {
+//     let pos = answer.split(",");
+//     pos = [parseInt(pos[0]), parseInt(pos[1])];
+//     let currPlayer = this.playerOneTurn ? "X" : "O";
+//     try {
+//       this.board.handleInput(currPlayer, pos);
+//     } catch(err) {
+//       console.log(err);
+//       this.board.handleInput(currPlayer, pos);
+//     }
+//     this.switchTurn();
+//     if (this.board.over) {
+//       console.log('gg');
+//     } else {
+//       this.board.render();
+//       this.run();
+//     }
+//   });
+// }
+
+Game.prototype.getInput = function() {
+
+  return new Promise( resolve => {
+    const prompt = rl.question('Enter a row: ', row => {
+        row = parseInt(row);
+        return rl.question('Enter a col: ', col => {
+          col = parseInt(col);
+          resolve([row, col]);
+        });
+    });
   });
+  // return await rl.question('Enter a pos: ', answer => {
+  //   let pos = answer.split(",");
+  //   console.log(pos);
+  //   return [parseInt(pos[0]), parseInt(pos[1])];
+  // });
+}
+
+Game.prototype.run = async function() {
+  let pos = await this.getInput();
+  console.log(pos);
+
+  let currPlayer = this.playerOneTurn ? "X" : "O";
+
+  this.board.handleInput(currPlayer, pos);
+  // try {
+  //   this.board.handleInput(currPlayer, pos);
+  // } catch(err) {
+  //   console.log(err);
+  //   this.board.handleInput(currPlayer, pos);
+  // }
+
+  this.switchTurn();
+
+  if (this.board.over) {
+    console.log('gg');
+  } else {
+    this.board.render();
+    this.run();
+  }
 }
 
 
