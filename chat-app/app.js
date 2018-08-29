6189a909d7af1fd085a4368343fcf90e9bf358d6
Timmy Jing <timmyjing@gmail.com>
2018-08-29 12:25:02 -0700

    Finished Phase 2 of Chat App

---
+++ chat-app/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile('index.html'));
+
+app.listen(3000, () => console.log('App is listening on 3000!'));
