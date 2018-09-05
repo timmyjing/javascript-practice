fd6903b53659ae6badbcc81dd1e7cde9487f4087
Timmy Jing <timmyjing@gmail.com>
2018-09-05 14:06:02 -0700

    Finished first pug template view

--- blog-party/app.js
+++ blog-party/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
+require('dotenv').config();
 
 const app = express();
 
-app.get('/', (req, res) => res.end('hello world'));
+app.use(morgan('combined'));
 
-app.listen(3000, () => console.log('listening on port 3000'));
+app.engine('pug', require('pug').__express)
+
+app.set('views', 'views');
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => res.render('hello', {variablePassed: 'jeff'}));
+
+app.listen(process.env.PORT, () => console.log(`i am listening on port ${process.env.PORT}`));
