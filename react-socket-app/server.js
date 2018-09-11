70d72c236db90a0d3621ab45f91d6bfb7c0e06c3
Timmy Jing <timmyjing@gmail.com>
2018-09-11 13:54:47 -0700

    Finished react-socket

--- react-socket-app/server.js
+++ react-socket-app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 
-const port = 3000;
+const port = 4000;
 
 const app = express();
 
@@ -13,10 +13,15 @@ const io = socketIO(server);
 io.on('connection', socket => {
   console.log('connected');
 
+  socket.on('change color', color => {
+    console.log('Color changed to ' + color);
+    io.sockets.emit('change color', color);
+  })
+
   socket.on('disconnect', () => {
     console.log('disconnected')
-  })
+  })  
 })
 
-server.listen(port, () => console.log('listening on port 3000'));  
+server.listen(port, () => console.log('listening on port 4000'));  
 
--- react-socket-app/socket-client/src/App.js
+++ react-socket-app/socket-client/src/App.js
@@ -6,11 +6,37 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      endpoint: `http://127.0.0.1:4000`,
+      color: 'white'
+    }
   }
+
+  send() {
+    const socket = socketIOClient(this.state.endpoint);
+    socket.emit('change color', this.state.color);
+  }
+
+  setColor(color) {
+    this.setState({color});
+  }
+
   render() {
+
+    const socket = socketIOClient(this.state.endpoint);
+
+    socket.on('change color', (color) =>  {
+      console.log('change');
+      document.body.style.backgroundColor = color;
+    });
+
     return (
       <div className="App">
-        <p>Chat App</p>
+        <button onClick={() => this.send()}>Change Color</button>
+
+        <button id="red" onClick={() => this.setColor('red')}>Red</button>
+        <button id="blue" onClick={() => this.setColor('blue')}>Blue</button>
+
       </div>
     );
   }
