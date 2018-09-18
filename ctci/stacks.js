1117a262bffd45afc68d7e0a687cee55a7fc3d54
Timmy Jing <timmyjing@gmail.com>
2018-09-18 14:21:01 -0700

    Finished 3.1 question

--- ctci/stacks.js
+++ ctci/stacks.js
@@ -65,3 +65,24 @@ class Queue {
   }
 
 }
+
+
+/*
+3.1 Describe how you could use a single array to implement three stacks.
+
+One way to approach this is to have a large array and keep track of the indexes where each stack starts and their end indexes. Have one 
+stack at the start, another at the mid, and the last at the end of the array going backwards. You can then
+key into the following index to push in a new element or pop an element by removing that stack's end index. This will be O(1) time since 
+you know the indexes but O(n) space and you have to initialize a large enough array. An issue with this will be when the array is full and
+everything will need to be resized. Another way would be to just push the elements into the array and maintain a separate array of indices
+for each stack...technically won't be using a single array anymore. 
+
+*/
+
+
+
+/*
+3.2 How would you design a stack which, in addition to push and pop, also has a
+function min which returns the minimum element? Push, pop and min should
+all operate in O(1) time.
+*/
