6086f09b3fc23a5351e0e31d7794ec5dea673924
Timmy Jing <timmyjing@gmail.com>
2018-11-06 14:49:37 -0800

    finished boggle pb

--- pairboarding/11-6-18.js
+++ pairboarding/11-6-18.js
@@ -76,3 +76,88 @@ function findAdj(arr, r, c, visited) {
 let arr =  [ [1,0,0,1,0], [1,0,1,0,0], [0,0,1,0,1], [1,0,1,0,1], [1,0,1,1,0] ];
 
 console.log(rivers(arr));
+
+
+/*
+Boggle Board (From Geeks For Geeks)
+Given a dictionary and an M x N board where every node has one character. Find all possible words that can be 
+formed by a sequence of adjacent characters. Note that we can move to any of 8 adjacent characters, but a word 
+should not have multiple instances of the same node.
+
+Example:
+
+// Input:
+let dictionary = ["GEEKS", "FOR", "QUIZ", "GO"];
+let boggle = [['G','I','Z'],
+              ['U','E','K'],
+              ['Q','S','E']];
+findWords(boggle) // => The Following words of dictionary are present
+                  // => GEEKS
+                  // => QUIZ
+
+what if large dictionary set? what if there were multiple words that started 
+with the same letter?
+
+starting with each node, if the node starts with a letter that is in the dictionary, 
+take those word and check if any of the adjacent has the next letter. if so, do a DFS on those nodes. if not, continue.
+once a word has been finished, then push it into the result. have to keep track of visited nodes for each word search.
+
+
+*/
+
+function solveBoggle(board, dictionary) {
+  const result = [];
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      let letter = board[i][j];
+
+      let words = dictionary.filter( el => el.startsWith(letter));
+
+      words.forEach(word => {
+        if (findWord(word, i, j, board)) result.push(word);
+      });
+    }
+  }
+
+  return result;
+}
+
+function findWord(word, i, j, board, visited) {
+  if (!word) return true;
+  if (i < 0 || i > board.length - 1) return false;
+  if (j < 0 || j > board.length - 1) return false;
+
+  if (!visited) {
+    visited = [];
+    for (let k = 0; k < board.length; k++) {
+      let row = new Array(board[0]);
+      row.fill(false);
+      visited.push(row);
+    }
+  }
+
+  visited[i][j] = true;
+
+  const letter = board[i][j];
+  const dirs = [ [-1,0], [1,0], [0,-1], [0, 1], [-1,-1], [-1, 1], [1, 1], [1, -1] ];
+
+  if (word.startsWith(letter)) {
+    for (let m = 0; m < dirs.length; m++) {
+      let dir = dirs[m];
+      if (findWord(word.slice(1), i + dir[0], j + dir[1], board, visited)) return true;
+    }
+
+  }
+
+
+  return false;
+}
+
+
+let dictionary = ["GEEKS", "FOR", "QUIZ", "GO"];
+let boggle = [['G','I','Z'],
+              ['U','E','K'],
+              ['Q','S','E']];
+
+console.log(solveBoggle(boggle, dictionary));
