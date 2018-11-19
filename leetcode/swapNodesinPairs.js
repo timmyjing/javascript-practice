70f224552dd6cd46cd33d9198fb473fa003cfced
Timmy Jing <timmyjing@gmail.com>
2018-11-18 23:15:49 -0800

    Finished swap adj nodes leetcode

---
+++ leetcode/swapNodesinPairs.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ 
+ 
+ iterate through nodes
+ take curr node next, save curr nodes next next pointer,
+ point curr node next -> curr node
+ curr node -> curr node next next;
+ have to keep track of of prev node;
+ prev would be curr node
+ curr node = curr node next next;
+ 
+ prev = 1
+ cn3
+ cnn4
+ cnnn - null
+ 4 - 3 ->
+ 
+ 2 -> 1 -> 4 - 3 -> null
+ 
+ if at head, rearrange head pointer
+ */
+var swapPairs = function(head) {
+  let prev = head;
+  let currNode = head;
+  
+  while (currNode) {
+    if (!currNode.next) return head;
+    let adj = currNode.next;
+    let adjNext = currNode.next.next;
+    adj.next = currNode;
+    currNode.next = adjNext;
+    if (prev === head) {
+      head = adj;
+    } else {
+      prev.next = adj;
+    }
+    prev = currNode;
+    currNode = currNode.next
+  }
+  
+  return head;
+};
